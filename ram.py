3eb964b7f699c2e13016510023a6c206c15bf74f
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2021-02-23 10:01:17 +0530

    Add

---
+++ ram.py
@@ -0,0 +1,21 @@
+import psutil
+def get_size(bytes,suffix='B'):
+    factor = 1024
+    for unit in ['','K','M','G','T','P']:
+        if bytes < factor:
+            return f'{bytes:.2f}{unit}{suffix}'
+        bytes /= factor
+
+#Get the swap memory details (if exists)
+system = psutil.virtual_memory()
+print (f'Total :{get_size(system.total)} ')
+print (f'Available :{get_size(system.available)}')
+print (f'Used :{get_size(system.used)}')
+print (f'percentage :{system.percent}%')
+
+swap = psutil.swap_memory()
+print ('\nSwap pertition:')
+print (f'Total: {get_size(swap.total)}')
+print (f'Free: {get_size(swap.free)}')
+print (f'Used: {get_size(swap.used)}')
+print (f'Percentage: {swap.percent}%')
